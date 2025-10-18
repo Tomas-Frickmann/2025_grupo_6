8b4a26727b8efaf416e7a2a7b403774076d5ef15
mordecool123 <lautigoni12@gmail.com>
2025-10-18 17:13:32 -0300

    comienzo de persistencia

---
+++ TP_TALLER_GOÑI_ESTEBAN_JULACHIS_FRICKMANN/src/testingDatos/TestPersistenciaBIN.java
@@ -0,0 +1,27 @@
+package testingDatos;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestPersistenciaBIN {
+
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+	}
+
+}
